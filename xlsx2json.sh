9e57bc25e7fa3caaf9bd3ccb961528eb332c9b5d
Sam <beardmans72@gmail.com>
2020-03-31 15:15:48 +1300

    Working on dockerfile for hosting site in container

--- xlsx2json.sh
+++ xlsx2json.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
+# Check if an override directory has been specified for the output files
+if [ "$1" != "" ]; then
+  outputDir=$1
+else
+  outputDir='data'
+fi
+
 # define variables
 confirmed="NZCOVID_confirmed_formatted.json"
 probable="NZCOVID_probable_formatted.json"
 # convert to json
-xlsx2csv data/NZ_COVID19_data.xlsx -f %Y/%m/%d -s 1 | tail -n +4 | csvjson | jq '.' > data/"$confirmed"
-xlsx2csv data/NZ_COVID19_data.xlsx -f %Y/%m/%d -s 2 | tail -n +4 | csvjson | jq '.' > data/"$probable"
+xlsx2csv "$outputDir/NZ_COVID19_data.xlsx" -f %Y/%m/%d -s 1 | tail -n +4 | csvjson | jq '.' > "$outputDir/$confirmed"
+xlsx2csv "$outputDir/NZ_COVID19_data.xlsx" -f %Y/%m/%d -s 2 | tail -n +4 | csvjson | jq '.' > "$outputDir/$probable"
 #/END
