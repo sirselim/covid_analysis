24e379e2c63bd96e78a92484fc33ffab1bc1c932
Sam <beardmans72@gmail.com>
2020-03-31 14:24:23 +1300

    Add '.' as jq wasn't working with redirection operator without a parameter

--- xlsx2json.sh
+++ xlsx2json.sh
@@ -3,6 +3,6 @@
 confirmed="NZCOVID_confirmed_formatted.json"
 probable="NZCOVID_probable_formatted.json"
 # convert to json
-xlsx2csv data/NZ_COVID19_data.xlsx -f %Y/%m/%d -s 1 | tail -n +4 | csvjson | jq > data/"$confirmed"
-xlsx2csv data/NZ_COVID19_data.xlsx -f %Y/%m/%d -s 2 | tail -n +4 | csvjson | jq > data/"$probable"
+xlsx2csv data/NZ_COVID19_data.xlsx -f %Y/%m/%d -s 1 | tail -n +4 | csvjson | jq '.' > data/"$confirmed"
+xlsx2csv data/NZ_COVID19_data.xlsx -f %Y/%m/%d -s 2 | tail -n +4 | csvjson | jq '.' > data/"$probable"
 #/END
