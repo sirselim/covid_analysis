239c5bb206c754951d84c05c11fc5b9073d53f0c
sirselim <miles.benton84@gmail.com>
2020-05-07 19:21:39 +1200

    fixed issues with data headers being changed

--- nz_covid_test_scrape.sh
+++ nz_covid_test_scrape.sh
@@ -8,12 +8,12 @@ sed 's/<\/TR[^>]*>/\n/Ig' | \
 sed 's/<\/\?\(TABLE\|TR\)[^>]*>//Ig' | \
 sed 's/^<T[DH][^>]*>\|<\/\?T[DH][^>]*>$//Ig' | sed 's/,//Ig' | \
 sed 's/<\/T[DH][^>]*><T[DH][^>]*>/,/Ig' | \
-grep -i -e 'Emergencies' -B 60 | \
-grep -i -e 'date,tests' -A 60 | \
+grep -i -e 'Emergencies' -B 80 | \
+grep -i -e 'date,tests' -A 80 | \
 grep -i -e 'Emergencies' -v | \
 sed 's/<th scope="col">//Ig' | 
 sed 's/^,//Ig' | sed 's/April/Apr/g' | sed -r 's/\<[0-9]{1,2}-[a-zA-Z]{3}\>/&-2020/' | \
-sed 's/Mar/03/g' | sed 's/Apr/04/g' | tr '-' '/' | \
+sed 's/Mar/03/g' | sed 's/Apr/04/g' | sed 's/May/05/g' | sed 's/Jun/06/g' | tr '-' '/' | \
 sed -E "s|([0-9]{1,2})/([0-9]{2})/([0-9]{4})|\3-\2-\1|" | tr '-' '/' | \
 sed 's/Date/Date of report/g' | sed 's/day/Day/g' | sed 's/tests/Tests/g' | sed 's/cumulative/Cumulative/g' | sed '2d' | \
 csvjson | jq > data/NZ_testing_stats.json
